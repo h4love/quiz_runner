a9845045ac92e8830450fc7a1eb71c9a2bf40345
Манчаккай М. В <mxmaster1203@live.com>
2018-05-12 20:05:54 +0700

    Topic search function added to database.c.

--- database.c
+++ database.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pcre.h>
+#include <locale.h>
 
 /*Чтение базы данных и вывод в файл
 Возвращает -1 при ошибке и 0 при успешном чтении
@@ -18,7 +20,7 @@ int read_db(FILE** file, char* filename)
 }
 
 /*Переключение строки и вывод в параметр string 
-Возвращает -1 при конце файла, в других случаях возвращает номер строки (index + 1)
+Возвращает -1 при конце файла, в других случаях возвращает номер строки (i + 1)
 Пример использования: i = get_str(&file, string, i)*/
 
 int next_str(FILE** file, char string[512], int i)
@@ -36,5 +38,71 @@ int next_str(FILE** file, char string[512], int i)
 		}
 		strcpy(string, tempstr);
 		return i+1;
+	}
+}
+
+/*Ищет название темы и номера строк с диапазоном вопросов 
+Возвращает -1 при ошибке и 0 при успешном выполнении
+В параметр char* string записывается название темы, в int* start начало вопросов по теме, в int* end конец строк с вопросами
+Пример использования: find_topic(&file, string, &a, &b)*/
+
+int find_topic(FILE** file, char* string, int* start, int* end)
+{
+	int i = 0, j, li, equal = 0;
+	char str[32];
+	char pattern[] = "<.*>"; //Маска регулярного выражения
+	setlocale (LC_ALL, (const char *) "ru."); //Кириллические символы
+	
+	while(equal < 2){
+		i = next_str(file, str, i);
+		
+   		pcre *re; //Переменная для результата компиляции
+   		int options = 0; //Опции компиляции
+   		int erroffset;
+   		const char *error;
+   		
+   		re = pcre_compile ((char *) pattern, options, &error, &erroffset, NULL); //Компилируем регулярное выражение с маской
+
+   		if (re) {
+      		int count = 0;
+      		int ovector[30]; //Задаём массив для символов
+     		count = pcre_exec (re, NULL, (char *) str, strlen(str), 0, 0, ovector, 30); //Проверяем на соответствие маске
+      		if (count) {
+        		for (int c = 0; c < 2 * count; c += 2){
+        		    if (ovector[c] >= 0) {
+        		    	if (equal == 1){ //Конец строк с вопросами
+               				*end = i-2;
+               				*start = *start+1;
+               				equal = 2;
+               			}
+               			if ((*start == (i-1)) && (equal == 0)){ //Начало строк с вопросами
+               				char temp[ovector[c+1]];
+        		    		for (j = 1; j < ovector[c+1]-1; j++){
+        		    			temp[j-1] = str[j];
+        		    		}
+        		    		temp[ovector[c+1]-2] = '\0';
+        		    		strcpy(string, temp); //Записываем название темы
+               				equal = 1;
+               			}
+            		}
+         		}
+      		}
+   		}
+   
+   		pcre_free((void *) re); //Высвобождаем занятую ранее память
+   		
+		if (i == -1){
+			if (equal == 1){
+				*end = li-1;
+			}
+			break;
+		}
+		li = i;
+	}
+	
+	if (equal == 2){
+		return 0;
+	} else {
+		return -1;
 	}
 }
--- database.h
+++ database.h
@@ -1,2 +1,3 @@
 int read_db(FILE**, char*);
 int next_str(FILE**, char*, int);
+int find_topic(FILE**, char*, int*, int*);
