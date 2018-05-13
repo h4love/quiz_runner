eaa4bd07e2483eebeb52fd2be7fa0baf892ae5e9
Манчаккай М. В <mxmaster1203@live.com>
2018-05-13 20:09:49 +0700

    A structure for questions has been added, in database.c included function for outputting questions from a string to structure. Code formatted.

--- database.c
+++ database.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <pcre.h>
 #include <locale.h>
+#include "database.h"
 
 /*Чтение базы данных и вывод в файл
 Возвращает -1 при ошибке и 0 при успешном чтении
@@ -12,7 +13,6 @@ int read_db(FILE** file, char* filename)
 {
     *file = fopen(filename, "r");
     if (file == NULL) {
-
         return -1;
     }
     else {
@@ -50,17 +50,18 @@ int next_str(FILE** file, char string[512], int i)
 
 int find_topic(FILE** file, char* string, int* start, int* end)
 {
+    fseek(*file, 0, SEEK_SET); //Перемотка в начало файла
     int i = 0, j, li, equal = 0;
-    char str[32];
+    char str[512];
     char pattern[] = "<.*>"; //Маска регулярного выражения
     setlocale(LC_ALL, (const char*)"ru."); //Кириллические символы
 
     while (equal < 2) {
         i = next_str(file, str, i);
-		if (i == -1) {
+        if (i == -1) {
             if (equal == 1) {
                 *end = li - 1;
-				*start = *start + 1;
+                *start = *start + 1;
             }
             break;
         }
@@ -82,7 +83,7 @@ int find_topic(FILE** file, char* string, int* start, int* end)
                             *end = i - 2;
                             *start = *start + 1;
                             equal = 2;
-							break;
+                            break;
                         }
                         if ((*start == (i - 1)) && (equal == 0)) { //Начало строк с вопросами
                             char temp[ovector[c + 1]];
@@ -96,8 +97,8 @@ int find_topic(FILE** file, char* string, int* start, int* end)
                     }
                 }
             }
-        }    
-		pcre_free((void*)re); //Высвобождаем занятую ранее память
+        }
+        pcre_free((void*)re); //Высвобождаем занятую ранее память
 
         li = i;
     }
@@ -108,4 +109,94 @@ int find_topic(FILE** file, char* string, int* start, int* end)
     else {
         return -1;
     }
+}
+
+/*Функция для вывода строк с вопросами в структуру qst
+Возвращает NULL при ошибке или адрес при успешном выполнении
+Параметр FILE** file принимает на вход файл с вопросами, int line указывает на строку с вопросом
+Пример использования: qst* now = get_question(&file, i)*/
+
+qst* get_question(FILE** file, int line)
+{
+    fseek(*file, 0, SEEK_SET); //Отматываем файл в начало
+    char str[512]; //Строка
+    int i = -1, index;
+    qst* p = malloc(sizeof(qst));
+
+    while (i != line) { //Пересчёт линий
+        i = next_str(file, str, i);
+        if (i == -1) {
+            break;
+        }
+    }
+
+    if (i != -1) { //Если линия найдена
+        int temp = sym_count(str, '|');
+        if (temp == 5) {
+            int id = 0;
+            int size = 0;
+            int step = 0;
+
+            //Вопрос
+
+            while (str[size + step] != '|') {
+                size++;
+            }
+            p->query = malloc((size) * sizeof(char));
+            while (id < size) {
+                p->query[id] = str[id + step];
+                id++;
+            }
+            p->query[id] = '\0';
+            step = size + 1;
+            id = 0;
+            size = 0;
+
+            //Ответы
+
+            for (index = 0; index < 4; index++) {
+                while (str[size + step] != '|') {
+                    size++;
+                }
+                p->a[index] = malloc((size + 1) * sizeof(char));
+                while (id < size) {
+                    p->a[index][id] = str[id + step];
+                    id++;
+                }
+                p->a[index][id] = '\0';
+                step = step + size + 1;
+                id = 0;
+                size = 0;
+            }
+
+            //Номер правильного ответа
+
+            p->right = str[step] - 48;
+
+            return p;
+        }
+        else { //Линия не соответствует формату
+            return NULL;
+        }
+    }
+    else { //Если линию найти не удалось
+        return NULL;
+    }
+}
+
+/*Функция для подсчёта кол-ва символов в строке
+Возвращает число символов
+Параметр char* origin - строка для поиска, char sym - искомый символ
+Пример использования: int temp = sym_count(str, '|')*/
+
+int sym_count(char* origin, char sym)
+{
+    int i = 0, count = 0;
+    while (origin[i] != '\0') {
+        if (origin[i] == sym) {
+            count++;
+        }
+        i++;
+    }
+    return count;
 }
--- database.h
+++ database.h
@@ -1,3 +1,11 @@
+typedef struct question {
+    char* query; //Вопрос
+    char* a[4]; //Ответы
+    int right; //Номер правильного
+} qst;
+
 int read_db(FILE**, char*);
 int next_str(FILE**, char*, int);
 int find_topic(FILE**, char*, int*, int*);
+qst* get_question(FILE**, int);
+int sym_count(char*, char);
